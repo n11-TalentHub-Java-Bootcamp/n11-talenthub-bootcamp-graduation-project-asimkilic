493022bc36caf4727c3772e2a48c3854784522c8
asimkilic <a.asim.kilic@gmail.com>
2022-01-19 11:06:29 +0000

    Project created

---
+++ loan-application/src/test/java/com/asimkilic/loan/application/LoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asimkilic.loan.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
