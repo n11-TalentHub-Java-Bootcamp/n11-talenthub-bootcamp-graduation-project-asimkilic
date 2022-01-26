73702bdabd321d5ba08bbd52446e3b85021ef530
asimkilic <a.asim.kilic@gmail.com>
2022-01-26 00:33:29 +0000

    added rolling file appender

--- loan-application/src/main/java/com/asimkilic/loan/application/exception/AsyncExceptionHandler.java
+++ loan-application/src/main/java/com/asimkilic/loan/application/exception/AsyncExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.asimkilic.loan.application.exception;
 
 import com.asimkilic.loan.application.twilio.TwilioSmsSender;
-import com.twilio.exception.ApiException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.stereotype.Component;
 
@@ -12,12 +12,15 @@ import java.util.Arrays;
 
 @Component
 public class AsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
-    private static final Logger LOGGER = LoggerFactory.getLogger(TwilioSmsSender.class);
+    private static final Logger logger = LogManager.getLogger(AsyncExceptionHandler.class);
+    //private static final Logger LOGGER = LoggerFactory.getLogger(TwilioSmsSender.class);
 
     // TODO : com.twilio.exception.ApiException daha spesifik handle et
     @Override
     public void handleUncaughtException(Throwable ex, Method method, Object... params) {
-        LOGGER.error("\n\t Exception: {}  \n\t  Method Name: {} \n\t  args: {}  ", ex, method.getName(), Arrays.toString(params));
+        //LOGGER.error("\n\t Exception: {}  \n\t  Method Name: {} \n\t  args: {}  ", ex, method.getName(), Arrays.toString(params));
+        logger.error("\n\t Exception: {}  \n\t  Method Name: {} \n\t  args: {}  ", ex, method.getName(), Arrays.toString(params));
+
     }
 
 }
